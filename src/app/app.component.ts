b14156b5652a4c68fdfdb9bfce1e9034768b734e
Prin Kammasitt <renovatio_@Prins-MacBook-Pro.local>
2018-11-13 15:35:59 +0700

    create new angular folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-dream-app';
+  title = 'fuck you';
+  condition = false;
+  show = true;
 }
